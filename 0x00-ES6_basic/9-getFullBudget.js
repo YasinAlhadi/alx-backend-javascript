90a5a4c11361cd9b78dac80daaf8d85b72038e54
YasinAlhadi <yasin.a.m.elhadi@gmail.com>
2023-04-19 02:20:24 +0200

    Return promise from fuction

---
+++ 0x00-ES6_basic/9-getFullBudget.js
@@ -0,0 +1,20 @@
+import getBudgetObject from './7-getBudgetObject.js';
+
+export default function getFullBudgetObject(income, gdp, capita) {
+    const getBudgetObject = (income, gdp, capita) => ({
+        income,
+        gdp,
+        capita,
+    });
+    const budget = getBudgetObject(income, gdp, capita);
+    const fullBudget = {
+        ...budget,
+        getIncomeInDollars() {
+            return `$${this.income}`;
+        },
+        getIncomeInEuros() {
+            return `${this.income} euros`;
+        },
+    };
+    return fullBudget;
+}
