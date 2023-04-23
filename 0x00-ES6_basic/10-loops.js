5ceca644ae2402238867ea254f933c2132a3dd0e
YasinAlhadi <yasin.a.m.elhadi@gmail.com>
2023-04-23 03:14:57 +0200

    for of loop

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,7 +1,7 @@
 export default function appendToEachArrayValue(array, appendString) {
-  for (const idx of array.keys()) {
-    const value = array[idx];
-    array[idx] = appendString + value;
+  for (const val of array) {
+    const idx = array.indexOf(val);
+    array[idx] = appendString + val;
   }
   return array;
 }
