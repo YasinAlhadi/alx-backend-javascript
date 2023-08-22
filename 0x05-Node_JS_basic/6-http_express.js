5da604b4a2f0005036bf4875dccfaf5fb2b4ee10
YasinAlhadi <yasin.a.m.elhadi@gmail.com>
2023-08-23 00:10:20 +0400

    small HTTP server using Express

--- 0x05-Node_JS_basic/6-http_express.js
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -2,13 +2,8 @@ const express = require('express');
 
 const app = express();
 
-const hostname = '127.0.0.1';
-const port = 1245;
-
 app.get('/', (req, res) => {
   res.send('Hello Holberton School!');
 });
 
-app.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+app.listen(1245);
