fc8b43cef64afcce343dc22db6937d069214bf1f
YasinAlhadi <yasin.a.m.elhadi@gmail.com>
2023-08-24 16:24:56 +0400

    fix code

--- 0x06-unittests_in_js/2-calcul_chai.test.js
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -1,112 +1,114 @@
-const chai = require('chai');
-const expect = chai.expect;
-const calculateNumber = require('./2-calcul');
+const { expect } = require('chai');
+const calculateNumber = require('./2-calcul_chai');
 
-describe('calculateNumber SUM', () => {
-    it('should return rounded value when type = SUM', () => {
-        expect(calculateNumber('SUM', 1, 3)).to.equal(4);
+describe('calculateNumber', () => {
+    describe('calculateNumber SUM', () => {
+        it('should return rounded value when type = SUM', () => {
+            expect(calculateNumber('SUM', 1, 3)).to.equal(4);
+        });
+
+        it('should return rounded value when type = SUM', () => {
+            expect(calculateNumber('SUM', 1, 3.7)).to.equal(5);
+        });
+
+        it('should return rounded value when type = SUM', () => {
+            expect(calculateNumber('SUM', 1.2, 3.7)).to.equal(5);
+        });
+
+        it('should return rounded value when type = SUM', () => {
+            expect(calculateNumber('SUM', 1.5, 3.7)).to.equal(6);
+        });
+
+        it('should return rounded value when type = SUM', () => {
+            expect(calculateNumber('SUM', 0.1, 0.3)).to.equal(0);
+        });
+
+        it('should return rounded value when type = SUM', () => {
+            expect(calculateNumber('SUM', 0.1, 0.3)).to.equal(0);
+        });
+
+        it('should return rounded value when type = SUM', () => {
+            expect(calculateNumber('SUM', -1, -3)).to.equal(-4);
+        });
+
+        it('should return rounded value when type = SUM', () => {
+            expect(calculateNumber('SUM', -1, -3.7)).to.equal(-5);
+        });
+
+        it('should return rounded value when type = SUM', () => {
+            expect(calculateNumber('SUM', -1.2, -3.7)).to.equal(-5);
+        });
+
+        it('should return rounded value when type = SUM', () => {
+            expect(calculateNumber('SUM', -1.5, -3.7)).to.equal(-5);
+        });
+
+        it('should return rounded value when type = SUM', () => {
+            expect(calculateNumber('SUM', 1, 0)).to.equal(1);
+        });
     });
 
-    it('should return rounded value when type = SUM', () => {
-        expect(calculateNumber('SUM', 1, 3.7)).to.equal(5);
+    describe('calculateNumber SUBTRACT', () => {
+        it('should return rounded value when type = SUBTRACT', () => {
+            expect(calculateNumber('SUBTRACT', 3, 1)).to.equal(2);
+        });
+
+        it('should return rounded value when type = SUBTRACT', () => {
+            expect(calculateNumber('SUBTRACT', 3.7, 1)).to.equal(3);
+        });
+
+        it('should return rounded value when type = SUBTRACT', () => {
+            expect(calculateNumber('SUBTRACT', 3.7, 1.2)).to.equal(3);
+        });
+
+        it('should return rounded value when type = SUBTRACT', () => {
+            expect(calculateNumber('SUBTRACT', 0.3, 0.1)).to.equal(0);
+        });
+
+        it('should return rounded value when type = SUBTRACT', () => {
+            expect(calculateNumber('SUBTRACT', -3, -1)).to.equal(-2);
+        });
+
+        it('should return rounded value when type = SUBTRACT', () => {
+            expect(calculateNumber('SUBTRACT', -3.7, -1)).to.equal(-3);
+        });
+
+        it('should return rounded value when type = SUBTRACT', () => {
+            expect(calculateNumber('SUBTRACT', -3.7, -1.2)).to.equal(-3);
+        });
+
+        it('should return rounded value when type = SUBTRACT', () => {
+            expect(calculateNumber('SUBTRACT', -3.7, -1.5)).to.equal(-3);
+        });
+
+        it('should return rounded value when type = SUBTRACT', () => {
+            expect(calculateNumber('SUBTRACT', 1, 0)).to.equal(1);
+        });
     });
 
-    it('should return rounded value when type = SUM', () => {
-        expect(calculateNumber('SUM', 1.2, 3.7)).to.equal(5);
-    });
+    describe('calculateNumber DIVIDE', () => {
+        it('should return rounded value when type = DIVIDE', () => {
+            expect(calculateNumber('DIVIDE', 9, 3)).to.equal(3);
+        });
 
-    it('should return rounded value when type = SUM', () => {
-        expect(calculateNumber('SUM', 1.5, 3.7)).to.equal(6);
-    });
+        it('should return rounded value when type = DIVIDE', () => {
+            expect(calculateNumber('DIVIDE', 3.7, 1)).to.equal(4);
+        });
 
-    it('should return rounded value when type = SUM', () => {
-        expect(calculateNumber('SUM', 0.1, 0.3)).to.equal(0);
-    });
+        it('should return rounded value when type = DIVIDE', () => {
+            expect(calculateNumber('DIVIDE', 3.7, 1.2)).to.equal(4);
+        });
 
-    it('should return rounded value when type = SUM', () => {
-        expect(calculateNumber('SUM', 0.1, 0.3)).to.equal(0);
-    });
+        it('should return rounded value when type = DIVIDE', () => {
+            expect(calculateNumber('DIVIDE', 3.7, 1.5)).to.equal(2);
+        });
 
-    it('should return rounded value when type = SUM', () => {
-        expect(calculateNumber('SUM', -1, -3)).to.equal(-4);
-    });
+        it('should return rounded value when type = DIVIDE', () => {
+            expect(calculateNumber('DIVIDE', -3, -1)).to.equal(3);
+        });
 
-    it('should return rounded value when type = SUM', () => {
-        expect(calculateNumber('SUM', -1, -3.7)).to.equal(-5);
-    });
-
-    it('should return rounded value when type = SUM', () => {
-        expect(calculateNumber('SUM', -1.2, -3.7)).to.equal(-5);
-    });
-
-    it('should return rounded value when type = SUM', () => {
-        expect(calculateNumber('SUM', -1.5, -3.7)).to.equal(-5);
-    });
-
-    it('should return rounded value when type = SUM', () => {
-        expect(calculateNumber('SUM', 1, 0)).to.equal(1);
-    });
-});
-
-describe('calculateNumber SUBTRACT', () => {
-    it('should return rounded value when type = SUBTRACT', () => {
-        expect(calculateNumber('SUBTRACT', 3, 1)).to.equal(2);
-    });
-
-    it('should return rounded value when type = SUBTRACT', () => {
-        expect(calculateNumber('SUBTRACT', 3.7, 1)).to.equal(3);
-    });
-
-    it('should return rounded value when type = SUBTRACT', () => {
-        expect(calculateNumber('SUBTRACT', 3.7, 1.2)).to.equal(3);
-    });
-
-    it('should return rounded value when type = SUBTRACT', () => {
-        expect(calculateNumber('SUBTRACT', 0.3, 0.1)).to.equal(0);
-    });
-
-    it('should return rounded value when type = SUBTRACT', () => {
-        expect(calculateNumber('SUBTRACT', -3, -1)).to.equal(-2);
-    });
-
-    it('should return rounded value when type = SUBTRACT', () => {
-        expect(calculateNumber('SUBTRACT', -3.7, -1)).to.equal(-3);
-    });
-
-    it('should return rounded value when type = SUBTRACT', () => {
-        expect(calculateNumber('SUBTRACT', -3.7, -1.2)).to.equal(-3);
-    });
-
-    it('should return rounded value when type = SUBTRACT', () => {
-        expect(calculateNumber('SUBTRACT', -3.7, -1.5)).to.equal(-3);
-    });
-
-    it('should return rounded value when type = SUBTRACT', () => {
-        expect(calculateNumber('SUBTRACT', 1, 0)).to.equal(1);
-    });
-});
-describe('calculateNumber DIVIDE', () => {
-    it('should return rounded value when type = DIVIDE', () => {
-        expect(calculateNumber('DIVIDE', 9, 3)).to.equal(3);
-    });
-
-    it('should return rounded value when type = DIVIDE', () => {
-        expect(calculateNumber('DIVIDE', 3.7, 1)).to.equal(4);
-    });
-
-    it('should return rounded value when type = DIVIDE', () => {
-        expect(calculateNumber('DIVIDE', 3.7, 1.2)).to.equal(4);
-    });
-
-    it('should return rounded value when type = DIVIDE', () => {
-        expect(calculateNumber('DIVIDE', 3.7, 1.5)).to.equal(2);
-    });
-
-    it('should return rounded value when type = DIVIDE', () => {
-        expect(calculateNumber('DIVIDE', -3, -1)).to.equal(3);
-    });
-
-    it('should return rounded value when type = DIVIDE', () => {
-        expect(calculateNumber('DIVIDE', -3.7, 0)).to.equal('Error');
+        it('should return rounded value when type = DIVIDE', () => {
+            expect(calculateNumber('DIVIDE', -3.7, 0)).to.equal('Error');
+        });
     });
 });
--- 0x06-unittests_in_js/6-payment_token.test.js
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -1,6 +1,6 @@
 const chai = require('chai');
 const expect = chai.expect;
-const getPaymentTokenFromAPI = require('./6-payment_token.js');
+const getPaymentTokenFromAPI = require('./6-payment_token');
 
 describe('getPaymentTokenFromAPI', () => {
     it('getPaymentTokenFromAPI returns the right token', (done) => {
