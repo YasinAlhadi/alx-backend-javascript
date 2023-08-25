25de3e3f6a70e9ef97304ad262d61d96e7a919f2
YasinAlhadi <yasin.a.m.elhadi@gmail.com>
2023-08-25 16:03:17 +0400

    read File

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,36 +1,34 @@
 const file = require('fs');
 
 function countStudents(filePath) {
-  const students = [];
-  const feilds = {};
-  let length = 0;
-  try {
-    const data = file.readFileSync(filePath, 'utf8');
-    const lines = data.split('\n');
-    length = lines.length;
-    for (const line of lines) {
-      if (line !== '') {
-        students.push(line.split(','));
-      }
-    }
-  } catch (err) {
+  if (!file.existsSync(filePath)) {
     throw new Error('Cannot load the database');
   }
-  length -= 1;
-  console.log(`Number of students: ${length}`);
+
+  let data = file.readFileSync(filePath, 'utf8');
+  data = data.split('\n');
+  const students = data.map((item) => item.split(','))
+    .filter((item) => item.length === 4 && item[0] !== 'firstname');
+
+  const obj = {
+    firstname: 0,
+    lastname: 1,
+    age: 2,
+    field: 3,
+  };
+
+  const CS = [];
+  const SWE = [];
+
   for (const student of students) {
-    if (!feilds[student[3]]) {
-      feilds[student[3]] = [];
-    }
-    feilds[student[3]].push(student[0]);
-  }
-  delete feilds.field;
-  for (const field in feilds) {
-    if (field) {
-      const list = feilds[field];
-      const count = list.length;
-      console.log(`Number of students in ${field}: ${count}. List: ${list.join(', ')}`);
+    if (student[obj.field] === 'CS') {
+      CS.push(student[obj.firstname]);
+    } else if (student[obj.field] === 'SWE') {
+      SWE.push(student[obj.firstname]);
     }
   }
+  console.log(`Number of students: ${students.length}`);
+  console.log(`Number of students in CS: ${CS.length}. List: ${CS.join(', ')}`);
+  console.log(`Number of students in SWE: ${SWE.length}. List: ${SWE.join(', ')}`);
 }
 module.exports = countStudents;
--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -1,37 +1,40 @@
 const file = require('fs').promises;
 
-function countStudents(filePath) {
-  const students = [];
-  const feilds = {};
-  let length = 0;
-  return file.readFile(filePath, 'utf8')
-    .then((data) => {
-      const lines = data.split('\n');
-      length = lines.length;
-      for (const line of lines) {
-        if (line !== '') {
-          students.push(line.split(','));
-        }
-      }
-      length -= 1;
-      console.log(`Number of students: ${length}`);
-      for (const student of students) {
-        if (!feilds[student[3]]) {
-          feilds[student[3]] = [];
-        }
-        feilds[student[3]].push(student[0]);
-      }
-      delete feilds.field;
-      for (const field in feilds) {
-        if (field) {
-          const list = feilds[field];
-          const count = list.length;
-          console.log(`Number of students in ${field}: ${count}. List: ${list.join(', ')}`);
-        }
-      }
-    })
-    .catch(() => {
-      throw new Error('Cannot load the database');
-    });
+async function countStudents(filePath) {
+  let data;
+  try {
+    data = await file.readFile(filePath, 'utf8');
+  } catch (err) {
+    throw new Error('Cannot load the database');
+  }
+  data = data.split('\n');
+
+  const students = data.map((item) => item.split(','))
+    .filter((item) => item.length === 4 && item[0] !== 'firstname');
+
+  const obj = {
+    firstname: 0,
+    lastname: 1,
+    age: 2,
+    field: 3,
+  };
+
+  const CS = [];
+  const SWE = [];
+
+  for (const student of students) {
+    if (student[obj.field] === 'CS') {
+      CS.push(student[obj.firstname]);
+    } else if (student[obj.field] === 'SWE') {
+      SWE.push(student[obj.firstname]);
+    }
+  }
+
+  console.log(`Number of students: ${students.length}`);
+  console.log(`Number of students in CS: ${CS.length}. List: ${CS.join(', ')}`);
+  console.log(`Number of students in SWE: ${SWE.length}. List: ${SWE.join(', ')}`);
+
+  return { students, CS, SWE };
 }
+
 module.exports = countStudents;
